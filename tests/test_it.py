423bd142428a31e1ac7c27857739c2f39a48247b
Kazuya Takei <myself@attakei.net>
2024-03-03 22:19:52 +0900

    ci: Refactor cases

--- tests/test_it.py
+++ tests/test_it.py
@@ -1,6 +1,8 @@
 """Standard integration tests."""
 
+import shutil
 from io import StringIO
+from pathlib import Path
 
 import pytest
 from bs4 import BeautifulSoup
@@ -12,22 +14,19 @@ def test__it(app: SphinxTestApp, status: StringIO, warning: StringIO):
     """Test to pass."""
 
 
-@pytest.mark.sphinx("html", confoverrides={"html_theme": "alabaster"})
-def test__work_on_bundle_theme(app: SphinxTestApp):
-    app.build()
-    soup = BeautifulSoup((app.outdir / "index.html").read_text(), "html.parser")
-    assert "hx-boost" in soup.body.attrs
+@pytest.mark.parametrize("theme", ["alabaster", "haiku", "furo", "sphinx_rtd_theme"])
+def test__work_on_theme(
+    sphinx_test_tempdir: Path, rootdir: Path, make_app: callable, theme: str
+):
+    testroot = "root"
+    srcdir = sphinx_test_tempdir / testroot
+    if not srcdir.exists():
+        testroot_path = rootdir / f"test-{testroot}"
+        shutil.copytree(testroot_path, srcdir)
 
-
-@pytest.mark.sphinx("html", confoverrides={"html_theme": "furo"})
-def test__work_on_custom_theme_1(app: SphinxTestApp):
-    app.build()
-    soup = BeautifulSoup((app.outdir / "index.html").read_text(), "html.parser")
-    assert "hx-boost" in soup.body.attrs
-
-
-@pytest.mark.sphinx("html", confoverrides={"html_theme": "sphinx_rtd_theme"})
-def test__work_on_custom_theme_2(app: SphinxTestApp):
+    app: SphinxTestApp = make_app(
+        "html", srcdir=srcdir, confoverrides={"html_theme": theme}
+    )
     app.build()
     soup = BeautifulSoup((app.outdir / "index.html").read_text(), "html.parser")
     assert "hx-boost" in soup.body.attrs

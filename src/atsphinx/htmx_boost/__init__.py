095e7b2ea3fa2c8ab14fb7c273914ca9d4a21576
Kazuya Takei <myself@attakei.net>
2024-03-03 23:36:34 +0900

    release: Bump version 0.0.0 -> 0.1.0

--- src/atsphinx/htmx_boost/__init__.py
+++ src/atsphinx/htmx_boost/__init__.py
@@ -5,7 +5,7 @@ from sphinx.application import Sphinx
 from sphinx.config import Config
 from sphinx.jinja2glue import BuiltinTemplateLoader
 
-__version__ = "0.0.0"
+__version__ = "0.1.0"
 
 
 class WithHtmxTemplateLoader(BuiltinTemplateLoader):  # noqa: D101

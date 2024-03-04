84dd35ec12b6cf2ab739a35633fab001362637b0
Kazuya Takei <myself@attakei.net>
2024-03-04 11:08:33 +0900

    ci: Correct test

--- tests/test_it.py
+++ tests/test_it.py
@@ -12,6 +12,7 @@ from sphinx.testing.util import SphinxTestApp
 @pytest.mark.sphinx("html")
 def test__it(app: SphinxTestApp, status: StringIO, warning: StringIO):
     """Test to pass."""
+    app.build()
 
 
 @pytest.mark.parametrize(

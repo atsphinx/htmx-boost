598b786ad2c3555d49c54a1bc225b4b20551fd62
Kazuya Takei <myself@attakei.net>
2024-03-20 00:55:41 +0900

    refactor: Guard event-handler by atsphinx-helper

--- src/atsphinx/htmx_boost/__init__.py
+++ src/atsphinx/htmx_boost/__init__.py
@@ -1,8 +1,8 @@
 """This is root of package."""
 
+from atsphinx.helper.decorators import emit_only
 from bs4 import BeautifulSoup
 from sphinx.application import Sphinx
-from sphinx.config import Config
 from sphinx.jinja2glue import BuiltinTemplateLoader
 
 __version__ = "0.1.4"
@@ -20,19 +20,17 @@ class WithHtmxTemplateLoader(BuiltinTemplateLoader):  # noqa: D101
         return soup.prettify()
 
 
-def setup_custom_loader(app: Sphinx, config: Config):
+@emit_only(formats=["html"])
+def setup_custom_loader(app: Sphinx):
     """Inject extra values about htmx-boost into generated config."""
-    # TODO: This extension must be loaded with html builders.
-    config.template_bridge = "atsphinx.htmx_boost.WithHtmxTemplateLoader"
-    if not hasattr(config, "html_js_files"):
-        config.html_js_files = []
-    # NOTE: Need control by config value?
-    config.html_js_files.append("https://unpkg.com/htmx.org@1.9.10")
+    app.config.template_bridge = "atsphinx.htmx_boost.WithHtmxTemplateLoader"
+    app.builder.init()
+    app.builder.add_js_file("https://unpkg.com/htmx.org@1.9.10")
 
 
 def setup(app: Sphinx):
     """Load as Sphinx-extension."""
-    app.connect("config-inited", setup_custom_loader)
+    app.connect("builder-inited", setup_custom_loader)
     return {
         "version": __version__,
         "env_version": 1,

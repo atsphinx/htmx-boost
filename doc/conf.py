c9f0119cd504f011b3b3aa7a9f586c332a2d81ca
Kazuya Takei <myself@attakei.net>
2024-03-20 02:07:51 +0900

    doc: Update for current features

--- doc/conf.py
+++ doc/conf.py
@@ -7,7 +7,11 @@ author = "Kazuya Takei"
 release = __version__
 
 # -- General configuration
-extensions = ["atsphinx.htmx_boost", "sphinx.ext.githubpages"]
+extensions = [
+    "atsphinx.htmx_boost",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.intersphinx",
+]
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
@@ -17,5 +21,18 @@ html_title = f"{project} v{release}"
 html_static_path = ["_static"]
 
 # -- Options for extensions
+# For sphinx.ext.intersphinx
+intersphinx_mapping = {
+    "sphinx": ("https://www.sphinx-doc.org/en/master", None),
+}
 # For atsphinx.htmx_boost
 htmx_boost_preload = "mouseover"
+
+
+def setup(app):
+    app.add_object_type(
+        "confval",
+        "confval",
+        objname="configuration value",
+        indextemplate="pair: %s; configuration value",
+    )

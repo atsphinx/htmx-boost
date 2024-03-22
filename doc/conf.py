2ae67e09e45984ba20d99b2c84f9606d14c3ff3d
Kazuya Takei <myself@attakei.net>
2024-03-23 03:26:13 +0900

    doc: Setup i18n

--- doc/conf.py
+++ doc/conf.py
@@ -1,3 +1,4 @@
+from atsphinx import mini18n
 from atsphinx.htmx_boost import __version__
 
 # -- Project information
@@ -9,16 +10,33 @@ release = __version__
 # -- General configuration
 extensions = [
     "atsphinx.htmx_boost",
+    "atsphinx.mini18n",
     "sphinx.ext.githubpages",
     "sphinx.ext.intersphinx",
 ]
-templates_path = ["_templates"]
+templates_path = ["_templates", mini18n.get_template_dir()]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for i18n
+gettext_compact = False
+language = "en"
+locale_dirs = ["_locales"]
 
 # -- Options for HTML output
 html_theme = "furo"
 html_title = f"{project} v{release}"
 html_static_path = ["_static"]
+html_sidebars = {
+    "**": [
+        "sidebar/scroll-start.html",
+        "sidebar/brand.html",
+        "mini18n/snippets/select-lang.html",
+        "sidebar/search.html",
+        "sidebar/navigation.html",
+        "sidebar/ethical-ads.html",
+        "sidebar/scroll-end.html",
+    ]
+}
 
 # -- Options for extensions
 # For sphinx.ext.intersphinx
@@ -27,6 +45,10 @@ intersphinx_mapping = {
 }
 # For atsphinx.htmx_boost
 htmx_boost_preload = "mouseover"
+# For atsphinx.mini18n,
+mini18n_default_language = "en"
+mini18n_support_languages = ["en", "ja"]
+mini18n_basepath = "/htmx-boost/"
 
 
 def setup(app):
